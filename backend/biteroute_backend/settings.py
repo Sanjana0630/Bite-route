0a85268705ee58148147309a174ecc216684eb5a
Sanjana0630 <sanjanamoralwar17@gmail.com>
2026-02-06 14:09:16 +0530

    Remove invalid admin creation code

--- backend/biteroute_backend/settings.py
+++ backend/biteroute_backend/settings.py
@@ -170,14 +170,3 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if os.getenv("RENDER") == "true":
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@example.com",
-            password="Admin@123"
-        )
